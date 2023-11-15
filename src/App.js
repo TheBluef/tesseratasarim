56bbc7fe406cc3b80147db7a16448806a04cd117
TheBluef <48221453+TheBluef@users.noreply.github.com>
2023-11-15 11:47:35 +0300

    contact link fix

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
             <Route path='/tesseratasarim/about' component={About} />
             <Route path='/tesseratasarim/shop' exact component={Shop} />
             <Route path='/tesseratasarim/shop/:id' component={ItemDetail} />
-            <Route path='/tesseratasarimnp/contact' component={Contact} />
+            <Route path='/tesseratasarim/contact' component={Contact} />
           </Switch>
           <div style={{ height: '35px', backgroundColor: 'white' }} />
         </div>
